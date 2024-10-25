3412aa6beb0658d09dceab74bf1f8b98b4acdb1d
Marlon Chavarria <marlonc@tevixmd.com>
2024-10-25 10:51:56 -0600

    php

---
+++ semana6/modulo4.php
@@ -0,0 +1,9 @@
+<?php
+$edad = 20;
+if($edad < 18){
+    echo "Eres menor de edad";
+}elseif ($edad == 18){
+    echo "Tienes 18";
+}else{
+    echo "Eres mayor de edad";
+}
---
+++ semana6/prework/index2.php
@@ -0,0 +1,6 @@
+<?php
+//conectarme a la BD, obtener la lista de tareas y crear dinamicamente el HTML que ocupo para mostrar las tareas
+    $nombre = "Diego";
+    $edad = 25;
+    echo "Nombre: $nombre, Edad: $edad";
+
