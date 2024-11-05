36a05a167749bff670676b674e650a9bbc060921
Marlon Chavarria <marlonc@tevixmd.com>
2024-11-05 09:13:00 -0600

    Se agrega la solucion del caso de estudio 1

--- semana7/estudio-caso1/app-tareas/js/dashboard.js
+++ semana7/estudio-caso1/app-tareas/js/dashboard.js
@@ -6,31 +6,44 @@ document.addEventListener('DOMContentLoaded', function () {
         id: 1,
         title: "Complete project report",
         description: "Prepare and submit the project report",
-        dueDate: "2024-12-01"
+        dueDate: "2024-12-01",
+        comments: [{ id: 1, description: "There is too many tasks pending" }, { id: 2, description: "It needs to be a complete report" }]
     },
     {
         id: 2,
         title: "Team Meeting",
         description: "Get ready for the season",
-        dueDate: "2024-12-01"
+        dueDate: "2024-12-01",
     },
     {
         id: 3,
         title: "Code Review",
         description: "Check partners code",
-        dueDate: "2024-12-01"
+        dueDate: "2024-12-01",
+
     },
     {
         id: 4,
         title: "Deploy",
         description: "Check deploy steps",
-        dueDate: "2024-12-01"
+        dueDate: "2024-12-01",
     }];
 
     function loadTasks() {
         const taskList = document.getElementById('task-list');
         taskList.innerHTML = '';
         tasks.forEach(function (task) {
+
+            let commentsList = '';
+            if (task.comments && task.comments.length > 0) {
+                commentsList = '<ul class="list-group list-group-flush">';
+                task.comments.forEach(comment => {
+                    commentsList += `<li class="list-group-item">${comment.description} 
+                    <button type="button" class="btn btn-sm btn-link remove-comment" data-visitid="${task.id}" data-commentid="${comment.id}">Remove</button>
+                    </li>`;
+                }); 
+                commentsList += '</ul>';
+            }
             const taskCard = document.createElement('div');
             taskCard.className = 'col-md-4 mb-3';
             taskCard.innerHTML = `
@@ -39,6 +52,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     <h5 class="card-title">${task.title}</h5>
                     <p class="card-text">${task.description}</p>
                     <p class="card-text"><small class="text-muted">Due: ${task.dueDate}</small> </p>
+                    ${commentsList}
+                     <button type="button" class="btn btn-sm btn-link add-comment"  data-id="${task.id}">Add Comment</button>
+
                 </div>
                 <div class="card-footer d-flex justify-content-between">
                     <button class="btn btn-secondary btn-sm edit-task"data-id="${task.id}">Edit</button>
@@ -55,6 +71,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
         document.querySelectorAll('.delete-task').forEach(function (button) {
             button.addEventListener('click', handleDeleteTask);
+        });
+
+        document.querySelectorAll('.add-comment').forEach(function (button) {
+            button.addEventListener('click', function (e) {
+                // alert(e.target.dataset.id);
+                document.getElementById("comment-task-id").value = e.target.dataset.id;
+                const modal = new bootstrap.Modal(document.getElementById("commentModal"));
+            modal.show()
+
+            })
+        });
+        document.querySelectorAll('.remove-comment').forEach(function (button) {
+            button.addEventListener('click', function (e) {
+                let taskId = parseInt(e.target.dataset.visitid);
+                let commentId = parseInt(e.target.dataset.commentid);
+                selectedTask = tasks.find(t => t.id === taskId);
+                commentIndex = selectedTask.comments.findIndex(c => c.id === commentId);
+                selectedTask.comments.splice(commentIndex,1);
+                loadTasks();
+            })
         });
     }
 
@@ -74,7 +110,7 @@ document.addEventListener('DOMContentLoaded', function () {
             //mostrar el modal
             const modal = new bootstrap.Modal(document.getElementById("taskModal"));
             modal.show();
-            
+
 
         } catch (error) {
             alert("Error trying to edit a task");
@@ -91,20 +127,42 @@ document.addEventListener('DOMContentLoaded', function () {
         loadTasks();
     }
 
+    document.getElementById('comment-form').addEventListener('submit', function (e){
+        e.preventDefault();
+        const comment = document.getElementById('task-comment').value;
+        const selectedTask = parseInt(document.getElementById('comment-task-id').value);
+        const task = tasks.find(t=> t.id === selectedTask);
+
+
+        let nextCommentId = 1;
+         
+        if(task.comments){
+            nextCommentId = task.comments.length + 1;
+        }else{
+            task.comments = [];
+        }
+        
+        task.comments.push({id: nextCommentId, description: comment});
+        const modal = bootstrap.Modal.getInstance(document.getElementById('commentModal'));
+        modal.hide();
+        loadTasks();
+
+    })
+
     document.getElementById('task-form').addEventListener('submit', function (e) {
         e.preventDefault();
-        
+
         const title = document.getElementById("task-title").value;
         const description = document.getElementById("task-desc").value;
         const dueDate = document.getElementById("due-date").value;
 
-        if(isEditMode){
+        if (isEditMode) {
             //todo editar
             const task = tasks.find(t => t.id === edittingId);
             task.title = title;
             task.description = description;
             task.dueDate = dueDate;
-        }else{
+        } else {
             const newTask = {
                 id: tasks.length + 1,
                 title: title,
@@ -117,10 +175,13 @@ document.addEventListener('DOMContentLoaded', function () {
         modal.hide();
         loadTasks();
     });
-    
 
-    document.getElementById('taskModal').addEventListener('show.bs.modal',function(){
-        if(!isEditMode){
+    document.getElementById('commentModal').addEventListener('show.bs.modal', function(){
+        document.getElementById('comment-form').reset();
+    })
+
+    document.getElementById('taskModal').addEventListener('show.bs.modal', function () {
+        if (!isEditMode) {
             document.getElementById('task-form').reset();
             // document.getElementById('task-title').value = "";
             // document.getElementById('task-desc').value = "";
@@ -128,7 +189,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    document.getElementById("taskModal").addEventListener('hidden.bs.modal', function(){
+    document.getElementById("taskModal").addEventListener('hidden.bs.modal', function () {
         edittingId = null;
         isEditMode = false;
     })
